37e246e850d6395d4a3d6e2006a9c6cff4c0d1e2
itsWuyu <itsWuyu@gmail.com>
2021-03-16 19:07:39 +0100

    Change code for ESP to send Telegramm messages

--- Safety/distance-safty/distance-safty.ino
+++ Safety/distance-safty/distance-safty.ino
@@ -1,20 +1,72 @@
-#include <HCSR04.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
+#include <UniversalTelegramBot.h>
+#include "NewPing.h"  
 #include <SPI.h>
 #include <RCSwitch.h>
 
 
+
 // Initialize sensor that uses digital pins 13 and 12.
-int triggerPin = 9;
-int echoPin = 10;
+int trigPin = D0;      // trigger pin
+int echoPin = D1;      // echo pin
 
 
-UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+NewPing sonar(trigPin, echoPin);
 RCSwitch mySwitch = RCSwitch();
 
+//------- WiFi Settings -------
+char ssid[] = "YOUR ID";       // your network SSID (name)
+char password[] = "YOUR PW";  // your network key
+
+#define BOTtoken "BOT ID" // Change it to your bot token
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+int Bot_mtbs = 1000; //mean time between scan messages
+long Bot_lasttime;   //last time messages' scan has been done
+
+
+void bot_setup() {
+  client.setInsecure(); // Required for ESP8266
+  const String commands = F("["
+    "{\"command\":\"help\",  \"description\":\"Get bot usage help\"},"
+    "{\"command\":\"start\", \"description\":\"Message sent when you open a chat with a bot\"},"
+    "{\"command\":\"status\",\"description\":\"Answer device current status\"}"  // no comma on last command
+  "]");
+  bot.setMyCommands(commands);
+  bot.sendMessage("CHAT ID", "system running!", "Markdown");
+}
+
+
+String ipAddress = "";
+ bool again = true;
+
+
 void setup () {
-    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
-    // Transmitter is connected to Arduino Pin #11  
-    mySwitch.enableTransmit(11);
+  Serial.begin(115200);
+  Serial.print("Connecting Wifi: ");
+  WiFi.begin(ssid, password); // Using stored credentials
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+     Serial.println("");
+   Serial.println("WiFi connected");
+   Serial.println("IP address: ");
+   IPAddress ip = WiFi.localIP();
+   Serial.println(ip);
+
+  ipAddress = ip.toString();
+    delay(500);
+  }
+  Serial.println("\r\nWiFi connected");
+  ArduinoOTA.begin();   // You should set a password for OTA. Ideally using MD5 hashes
+  bot_setup();
+
+     
+   // Transmitter is connected to Arduino Pin #11  
+    mySwitch.enableTransmit(D3);
 
     // Optional set pulse length.
     mySwitch.setPulseLength(505);
@@ -25,21 +77,53 @@ void setup () {
     // Optional set number of transmission repetitions.
     //mySwitch.setRepeatTransmit(5);
 
+  /// First measurement are sometimes off so we do these befor the real code starts
+   for (int i = 0; i < 10; i++){
+      sonar.ping_cm();    
+      delay(50);
+    }
+
+   
 }
 
+
+
 void loop () {
+
+    ArduinoOTA.handle(); 
+    
     // Every 100 miliseconds, do a measurement using the sensor and print the distance in centimeters.
-    double distance = distanceSensor.measureDistanceCm();
+    double distance = sonar.ping_cm();
+
+   
+ 
+  
 
     if (distance <= 10 && distance > 0 ){
-       Serial.println(distance);
-       delay(100);
+       again = true;
+       Serial.print("Distance: ");
+       Serial.print(distance, 1); Serial.println(" cm");
+       delay(1000);
        
       
       }
-   else {
-    Serial.println("ERROR");
-    mySwitch.send("001010110000111011100111");
+   else if((distance > 10 || distance <= 0) && again == true ) {
+    for (int i = 0; i < 3 ; i++){
+      Serial.println("ERROR ");
+      Serial.println(i+1);
+      Serial.print("Distance: ");
+      Serial.print(distance, 1); Serial.println(" cm");
+      mySwitch.send("001010110000111011100111");
+      bot.sendMessage("CHAT ID1", "SCHLAUCH AB !! ERROR !! PUMPE DEAKTIVIERT !!  ", "Markdown");
+      delay(5000);
     }
+    again = false;
+    Serial.println("False");
+    }
+
+    else{
+      }
+      
+      
 
 }
